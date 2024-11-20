7ec7d2f43627b44ccec97f1a03b77b49fc7dc18f
Robert Medina <66793722+da-b0ss@users.noreply.github.com>
2024-11-20 00:47:29 -0500

    script n stuff

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
-import express from express;
+import express from "express"; 
 
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("server is ready");
+});
 
 app.listen(5000, () => {
     console.log("Server started @ http://localhost:5000");
