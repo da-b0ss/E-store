313a653faf31826a5b67d2ceca7ad953e23f33db
Robert Medina <66793722+da-b0ss@users.noreply.github.com>
2024-11-20 11:16:39 -0500

    creates product

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,46 @@
 import express from "express"; 
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+import Product from "./models/product.model.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json()); //allows us to accept JSON data in the req.body
+
+//app.get("/products", (req, res) => {});
+app.post("/api/products", async (req, res) => {
+    const product = req.body; // user will send this data 
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({success:false, message:"Provide all fields"});
+    }
+
+    const newProduct = new Product(product);
+
+    try{
+        await newProduct.save();
+        res.status(201).json({success:true, data:newProduct});
+    } catch(error){
+        console.error("Error in create product:", error.message);
+        res.status(500).json({success:false,message:"Server error"});
+    }
+});
+
+//Postman desktop app
+
+/*app.get("/", (req, res) => {
     res.send("server is ready");
+});*/
+
+//console.log(process.env.MONGO_URI);
+app.delete("/api/products/:id", async (req, rest) => {
+    const {id} = req.params;
+    console.log("id:", id);
 });
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started @ http://localhost:5000");
 });
