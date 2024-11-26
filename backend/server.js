f636e7e61cc6483fc7fa822031d7772ab702df72
Robert Medina <66793722+da-b0ss@users.noreply.github.com>
2024-11-26 12:01:37 -0500

    front-back integration

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,11 @@ app.use(express.json()); //allows us to accept JSON data in the req.body
 app.use("/api/products", productRoutes);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend1/dist")));
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
 }
 
 app.listen(PORT, () => {
