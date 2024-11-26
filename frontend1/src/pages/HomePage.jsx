933a0ca51619948039d242daff48e878dfdd1d08
Robert Medina <66793722+da-b0ss@users.noreply.github.com>
2024-11-26 10:12:54 -0500

    product store setup

--- frontend1/src/pages/HomePage.jsx
+++ frontend1/src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
 import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
+import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
+
+  const {fetchProducts, products} = useProductStore();
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log("products", products);
+
 	return (
 		<Container maxW='container.xl' py={12}>
 			<VStack spacing={8}>
@@ -15,6 +25,21 @@ const HomePage = () => {
 					Current Products 🚀
 				</Text>
 
+        <SimpleGrid 
+          columns={{ 
+            base: 1, 
+            md: 2, 
+            lg: 3 
+          }} 
+          spacing={10}
+          w={"full"}
+        >
+          {products.map((product) => (
+            <ProductCard key = {product._id} product = {product}/>
+          ))}
+        </SimpleGrid>
+
+        
 					<Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
 						No products found 😢{" "}
 						<Link to={"/create"}>	
