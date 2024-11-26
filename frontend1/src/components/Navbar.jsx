1d29459ebd2474d6af707510082bf058620aec04
Robert Medina <66793722+da-b0ss@users.noreply.github.com>
2024-11-26 05:33:51 -0500

    colormode setup

--- frontend1/src/components/Navbar.jsx
+++ frontend1/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import { Button, Container, Flex, HStack, Text, useColorMode } from "@chakra-ui/react"
 import { Link } from "react-router-dom"
-
+import { PlusSquareIcon } from '@chakra-ui/icons'
+import { IoMoon } from "react-icons/io5"
+import { LuSun } from "react-icons/lu"
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
     <Container maxW = {"1140px"} px={4}>
       <Flex 
@@ -30,9 +34,13 @@ const Navbar = () => {
         <HStack spacing={2} alignItems={"center"}>
           <Link to="/create">
             <Button>
-              {/*<PlusSquareIcon />*/}
+              <PlusSquareIcon fontSize={20} />
             </Button>
           </Link>
+
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
+          </Button>
         </HStack>
 
       </Flex>
